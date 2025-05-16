84f012ed71d0f7775921a98853d3202bb3994320
Oscar <190858256+Oscar-74@users.noreply.github.com>
2025-05-16 17:28:37 +0200

    update 16/05

--- src/features.c
+++ src/features.c
@@ -12,5 +12,47 @@
  */
 
 void helloWorld() {
-    printf("Hello World !");
+    printf("Hello World !!!!!");
+}
+void dimension(char*source_path) {
+    unsigned char* data = NULL;
+    int w=0, h=0, n=0;
+    read_image_data(source_path, &data, &w, &h, &n);
+    printf("dimension : %d, %d\n", w, h);
+}
+void first_pixel(char*source_path){
+    unsigned char* data = NULL;
+    int w, h, n;
+    
+    read_image_data(source_path, &data, &w, &h, &n);
+    
+    unsigned char R = data[0];
+    unsigned char G = data[1];
+    unsigned char B = data[2];
+
+    printf("first_pixel: %d, %d, %d", R, G, B);
+}
+void tenth_pixel(char*source_path){
+    unsigned char* data = NULL;
+    int w, h, n;
+    
+    read_image_data(source_path, &data, &w, &h, &n);
+    
+    unsigned char R = data[27];
+    unsigned char G = data[28];
+    unsigned char B = data[29];
+
+    printf("tenth_pixel: %d, %d, %d", R, G, B);
+}
+void second_line(char*source_path){
+    unsigned char* data = NULL;
+    int w, h, n;
+    
+    read_image_data(source_path, &data, &w, &h, &n);
+    
+    unsigned char R = data[4464];
+    unsigned char G = data[4465];
+    unsigned char B = data[4466];
+
+    printf("second_line: %d, %d, %d", R, G, B);
 }
