7a8e06f2bf56fa2afd07d4a1ea2b46e217cd0a60
Hugo-F1 <190857804+Hugo-F1@users.noreply.github.com>
2025-06-04 14:13:59 +0200

    Modif 04/06 2

--- src/utils.c
+++ src/utils.c
@@ -2,20 +2,6 @@
 #include <stddef.h>
 #include "utils.h"
 
-pixelRGB * get_pixel( unsigned char* data, const unsigned int w, const unsigned int h, const unsigned int n, const unsigned int x, const unsigned int y ){
-    
-    read_image_data(source_path, &data, &w, &h, &n);
-    
-    if ((x>w)|| (y>h) || (&data == NULL)){
-        return NULL;
-    }
-
-    return (pixelRGB *) &data[(y*w+x)*n] ;
-}
-
-void print_pixel( char *filename, int x, int y ){
-    
-}
 
 
 pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ){
