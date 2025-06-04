3fbac4a2c6f2b37e07462bd67e0da7a19a69e13c
Hugo-F1 <190857804+Hugo-F1@users.noreply.github.com>
2025-06-04 14:00:18 +0200

    modif 04/06 1

--- src/utils.c
+++ src/utils.c
@@ -2,8 +2,19 @@
 
 #include "utils.h"
 
-/**
- * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
- */
+pixelRGB * get_pixel( unsigned char* data, const unsigned int w, const unsigned int h, const unsigned int n, const unsigned int x, const unsigned int y ){
+    
+    read_image_data(source_path, &data, &w, &h, &n);
+    
+    if ((x>w)|| (y>h) || (&data == NULL)){
+        return NULL;
+    }
+
+    return (pixelRGB *) &data[(y*w+x)*n] ;
+}
+
+void print_pixel( char *filename, int x, int y ){
+    
+}
+
 
